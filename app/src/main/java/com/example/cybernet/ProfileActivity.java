62c55ef2c88d39807bb340cbdffa67eab018cb93
immanuelcooke <immanuel.cooke@gmail.com>
2023-02-15 09:02:25 +0000

    Added profile picture functionality and age variable to database, also setup cloud storage to store the images 14/02/2023

--- app/src/main/java/com/example/cybernet/ProfileActivity.java
+++ app/src/main/java/com/example/cybernet/ProfileActivity.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -17,6 +18,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -24,8 +27,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
+import java.util.UUID;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -34,6 +41,7 @@ public class ProfileActivity extends AppCompatActivity {
     private String userID;
 
     private Button logout;
+    private Button changePicture;
     private ImageView profilePicture;
     private Uri imagePath;
 
@@ -52,6 +60,14 @@ public class ProfileActivity extends AppCompatActivity {
                 startActivityForResult(photoIntent,1);
 
 
+            }
+        });
+
+        changePicture = (Button) findViewById(R.id.changePicture);
+        changePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadImage();
             }
         });
 
@@ -119,6 +135,46 @@ public class ProfileActivity extends AppCompatActivity {
             e.printStackTrace();
         }
         profilePicture.setImageBitmap(bitmap);
+    }
+
+    private void uploadImage(){
+
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading...");
+        progressDialog.show();
+
+        // This generates a random string which is the name of the photo being uploaded this prevents overwriting other photos which could have the same name
+        FirebaseStorage.getInstance().getReference("images/"+ UUID.randomUUID().toString()).putFile(imagePath).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()){
+                    task.getResult().getStorage().getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Uri> task) {
+                            if (task.isSuccessful()){
+                                updateProfilePicture(task.getResult().toString());
+                            }
+                        }
+                    });
+                    progressDialog.dismiss();
+                    Toast.makeText(ProfileActivity.this, "Image Uploaded Successfully!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(ProfileActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                }
+                progressDialog.dismiss();
+            }
+        }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                double progress = 100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount();
+                progressDialog.setMessage(" Uploaded "+(int) progress + "%");
+            }
+        });
+    }
+
+
+    private void updateProfilePicture(String url){
+        FirebaseDatabase.getInstance().getReference("Users/"+FirebaseAuth.getInstance().getCurrentUser().getUid() + "/profilePicture").setValue(url);
     }
 
 }
